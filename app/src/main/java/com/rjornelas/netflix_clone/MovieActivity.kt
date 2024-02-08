0d47977fc0ca504410c5709b6348aa64e8a94320
rjornelas <rafael.j.ornelas@gmail.com>
2024-02-08 09:54:46 -0300

    Created shading on the movie image for the movie details screen

--- app/src/main/java/com/rjornelas/netflix_clone/MovieActivity.kt
+++ app/src/main/java/com/rjornelas/netflix_clone/MovieActivity.kt
@@ -1,8 +1,12 @@
 package com.rjornelas.netflix_clone
 
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.LayerDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 
 class MovieActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +18,11 @@ class MovieActivity : AppCompatActivity() {
 
         supportActionBar?.setHomeAsUpIndicator(R.drawable.baseline_arrow_back_24)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val layerDrawable: LayerDrawable = ContextCompat.getDrawable(this, R.drawable.shadows) as LayerDrawable
+        val movieCover = ContextCompat.getDrawable(this, R.drawable.movie_batman)
+        layerDrawable.setDrawableByLayerId(R.id.cover_drawable, movieCover)
+        val coverImg: ImageView = findViewById(R.id.movie_img)
+        coverImg.setImageDrawable(layerDrawable)
     }
 }
