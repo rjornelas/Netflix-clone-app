82ad1433b300fce5b84d083cec78d2329274f4cc
rjornelas <rafael.j.ornelas@gmail.com>
2024-02-08 12:27:58 -0300

    Synchronizing ScrollView with RecyclerView and adding image grid in the Similar options section

--- app/src/main/java/com/rjornelas/netflix_clone/MovieActivity.kt
+++ app/src/main/java/com/rjornelas/netflix_clone/MovieActivity.kt
@@ -1,23 +1,46 @@
 package com.rjornelas.netflix_clone
 
-import android.graphics.drawable.Drawable
 import android.graphics.drawable.LayerDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.GridLayout
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.rjornelas.netflix_clone.model.Movie
 
 class MovieActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie)
 
+        val txtTitle: TextView = findViewById(R.id.tv_movie_title)
+        val txtSynopsis: TextView = findViewById(R.id.tv_movie_synopsis)
+        val txtCast: TextView = findViewById(R.id.tv_cast)
+        val rv: RecyclerView = findViewById(R.id.rv_similar)
+
+        txtTitle.text = "Batman"
+        txtSynopsis.text = getString(R.string.synopsis)
+        txtCast.text = getString(R.string.cast, "Actor A, Actor B, Actress A, Actress B")
+
+        val movies = mutableListOf<Movie>()
+        for (i in 0 until 30){
+            val movie = Movie(R.drawable.movie_batman)
+            movies.add(movie)
+        }
+
+        rv.layoutManager = GridLayoutManager(this, 3)
+        rv.adapter = MovieAdapter(movies, R.layout.movie_item_similar)
+
         val toolbar: Toolbar = findViewById(R.id.movie_toolbar)
         setSupportActionBar(toolbar)
 
         supportActionBar?.setHomeAsUpIndicator(R.drawable.baseline_arrow_back_24)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = null
 
         val layerDrawable: LayerDrawable = ContextCompat.getDrawable(this, R.drawable.shadows) as LayerDrawable
         val movieCover = ContextCompat.getDrawable(this, R.drawable.movie_batman)
